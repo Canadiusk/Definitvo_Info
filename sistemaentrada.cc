c406ebde17776dfdb53f46a432b4aed2dc03d6b2
Canadiusk <39306982+Canadiusk@users.noreply.github.com>
2018-05-31 19:36:39 +0200

    Actualización de structs

--- sistemaentrada.cc
+++ sistemaentrada.cc
@@ -1,1 +1,32 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+
+struct Asientos{
+string zona;
+int numeroasiento;
+vector <string> nombrecompleto;
+int fecha; //DDMMAAAA
+};
+
+struct Precio{
+int precio;
+string zona;
+int jornada;
+string nivel;
+Asientos localizacion;
+};
+
+struct Socio{
+int numerosocio;
+string nombre;
+vector <string> nombrecompleto;
+};
+
+
+
+
+
+
